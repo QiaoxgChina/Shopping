aa1c7b5c5142657076426c31e608b988ef94c2dc
QiaoxgChina <qxg.bxf@163.com>
2018-04-23 10:37:42 +0800

    add third module

---
+++ third/src/main/java/com/example/third/listener/ThirdLoginListener.java
@@ -0,0 +1,13 @@
+package com.example.third.listener;
+
+/**
+ * Created by Administrator on 2018/4/16.
+ */
+
+public interface ThirdLoginListener {
+
+    void onLoginSuccess();
+
+    void onLoginFail();
+
+}
